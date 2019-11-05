4b34e354e6d2f2467576efb51e51f90c8b72360c
Matheus Cendrão <matheus.cendrao@plataformatec.com.br>
2019-11-05 16:01:47 -0300

    Add match to Tesla.Env in the adapter

--- lib/tesla_adapter.ex
+++ lib/tesla_adapter.ex
@@ -8,10 +8,10 @@ defmodule GlassFactoryApi.TeslaAdapter do
   @impl HttpAdapter
   def get(path, headers) do
     case Tesla.get(path, headers: headers) do
-      {:ok, %{status: 200, body: body, headers: headers}} ->
+      {:ok, %Tesla.Env{status: 200, body: body, headers: headers}} ->
         {:ok, %{status_code: 200, body: body, headers: headers}}
 
-      {:ok, %{status: status, body: body, headers: headers}} ->
+      {:ok, %Tesla.Env{status: status, body: body, headers: headers}} ->
         {:error, %{status_code: status, body: body, headers: headers}}
 
       {:error, reason} ->
