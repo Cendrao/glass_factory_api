309d208339480faa1cb6aafaa2dd4e184234afbc
Paulo Henrique da Silva <paulo.silva@plataformatec.com.br>
2019-11-14 10:47:58 -0300

    Apply @Cendrao suggestion.

--- lib/clients/reports.ex
+++ lib/clients/reports.ex
@@ -7,8 +7,8 @@ defmodule GlassFactoryApi.Clients.Reports do
 
   ## Examples
 
-      iex> GlassFactoryApi.Clients.Reports(2079)
-      [ %TimeReport{
+      iex> GlassFactoryApi.Clients.Reports.list_time_reports(2079)
+      [:ok, %TimeReport{
           client_id: 2079,
           project_id: 14330,
           job_id: nil,
@@ -21,7 +21,7 @@ defmodule GlassFactoryApi.Clients.Reports do
         }
       ]
   """
-  @spec list_time_reports(integer(), map()) :: {:error, String.t()} | {:ok, [TimeReport]}
+  @spec list_time_reports(integer(), map()) :: {:error, String.t()} | {:ok, [TimeReport.t()]}
   def list_time_reports(client_id, config \\ %{}) do
     client_request = ApiClient.get("clients/#{client_id}/reports/time", config)
 
@@ -36,6 +36,31 @@ defmodule GlassFactoryApi.Clients.Reports do
     end
   end
 
+  @doc """
+  List time reports from the given client id or raises an error if something went wrong.
+
+  Same of `list_time_reports\2` but raises instead of returning a tuple with `:error`
+
+  ## Examples
+
+      iex> GlassFactoryApi.Clients.Reports.list_time_reports!(2079)
+      [ %TimeReport{
+          client_id: 2079,
+          project_id: 14330,
+          job_id: nil,
+          activity_id: nil,
+          user_id: 3,
+          role_id: 1480,
+          date: "2018-07-10",
+          planned: 8,
+          time: 0
+        }
+      ]
+
+      iex> GlassFactoryApi.Clients.Reports.list_time_reports!(999)
+      ** (RuntimeError) Can't find time reports for client id 999
+  """
+  @spec list_time_reports!(integer, map) :: [TimeReport.t()]
   def list_time_reports!(client_id, config \\ %{}) do
     with {:ok, time_reports} <- list_time_reports(client_id, config) do
       time_reports
