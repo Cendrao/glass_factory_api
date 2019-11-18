f404ac374863e1fc210d9f25dd428f451dd3aba9
Alan Nascimento <alan.nascimento@plataformatec.com.br>
2019-11-18 12:29:34 -0300

    Update the get_client and get_client! function docs

--- lib/clients.ex
+++ lib/clients.ex
@@ -6,18 +6,25 @@ defmodule GlassFactoryApi.Clients do
   alias GlassFactoryApi.Clients.Client
 
   @doc """
-  Get a client given the id
+  Return a tuple with `:ok` atom and the client from the specified ID
+  or an tuple with `:error` and a string with the error description.
 
   ## Examples
 
-      iex> GlassFactoryApi.Clients.get_client("1234")
-      %Client{
-        id: 1234,
-        name: "Google",
-        archived_at: null,
-        owner_id: 567,
-        office_id: 789
-      }
+  iex> GlassFactoryApi.Clients.get_client("1234")
+  {:ok,
+    %Client{
+      id: 1234,
+      name: "Google",
+      archived_at: null,
+      owner_id: 567,
+      office_id: 789
+    }
+  }
+
+
+  iex> GlassFactoryApi.Clients.get_client("1")
+  {:error, "Can't find a client with id 1"}
   """
   @spec get_client(integer(), map) :: {atom(), Client.t() | String.t()}
   def get_client(client_id, config \\ %{}) do
@@ -25,11 +32,31 @@ defmodule GlassFactoryApi.Clients do
       {:ok, Client.to_struct(body)}
     else
       {:ok, %{status: 404}} -> {:error, "Can't find a client with id #{client_id}"}
-      {:ok, %{body: body}}  -> {:error, body}
+      {:ok, %{body: body}} -> {:error, body}
       {:error, error} -> {:error, error}
     end
   end
 
+  @doc """
+  Return the client from the specified ID
+  or raises an error.
+
+  ## Examples
+
+  iex> GlassFactoryApi.Clients.get_client("1234")
+  %Client{
+    id: 1234,
+    name: "Google",
+    archived_at: null,
+    owner_id: 567,
+    office_id: 789
+  }
+
+
+  iex> GlassFactoryApi.Clients.get_client("1")
+  "Can't find a client with id 1"
+  """
+  @spec get_client!(integer(), map) :: {Client.t()}
   def get_client!(client_id, config \\ %{}) do
     with {:ok, client} <- get_client(client_id, config) do
       client
@@ -44,28 +71,28 @@ defmodule GlassFactoryApi.Clients do
 
   ## Examples
 
-      iex> GlassFactoryApi.Clients.list_clients()
-      {:ok,
-        [
-          %Client{
-            id: 1234,
-            name: "Google",
-            archived_at: null,
-            owner_id: 567,
-            office_id: 789
-          },
-          %Client{
-            id: 1235,
-            name: "Facebook",
-            archived_at: null,
-            owner_id: 567,
-            office_id: 789
-          }
-        ]
+  iex> GlassFactoryApi.Clients.list_clients()
+  {:ok,
+    [
+      %Client{
+        id: 1234,
+        name: "Google",
+        archived_at: null,
+        owner_id: 567,
+        office_id: 789
+      },
+      %Client{
+        id: 1235,
+        name: "Facebook",
+        archived_at: null,
+        owner_id: 567,
+        office_id: 789
       }
+    ]
+  }
 
-      iex> GlassFactoryApi.Clients.list_clients()
-      {:error, "econnrefused"}
+  iex> GlassFactoryApi.Clients.list_clients()
+  {:error, "econnrefused"}
   """
 
   @spec list_clients(map()) :: {atom(), [Client.t()] | String.t()}
@@ -83,27 +110,26 @@ defmodule GlassFactoryApi.Clients do
 
   ## Examples
 
-      iex> GlassFactoryApi.Clients.list_clients!()
-      [
-        %Client{
-          id: 1234,
-          name: "Google",
-          archived_at: null,
-          owner_id: 567,
-          office_id: 789
-        },
-        %Client{
-          id: 1235,
-          name: "Facebook",
-          archived_at: null,
-          owner_id: 567,
-          office_id: 789
-        }
-      ]
+  iex> GlassFactoryApi.Clients.list_clients!()
+  [
+    %Client{
+      id: 1234,
+      name: "Google",
+      archived_at: null,
+      owner_id: 567,
+      office_id: 789
+    },
+    %Client{
+      id: 1235,
+      name: "Facebook",
+      archived_at: null,
+      owner_id: 567,
+      office_id: 789
+    }
+  ]
 
-      iex> GlassFactoryApi.Clients.list_clients!()
-      "econnrefused"
-
+  iex> GlassFactoryApi.Clients.list_clients!()
+  "econnrefused"
   """
 
   @spec list_clients!(map()) :: {[Client.t()]}
