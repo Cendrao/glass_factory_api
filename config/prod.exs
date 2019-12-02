dc6ae8a9db8b44c87b25eb8a187ee917d92aca53
João Britto <jabcalves@gmail.com>
2019-12-02 14:54:26 -0300

    Remove mix config: no need for it in the library itself.

--- config/prod.exs
+++
@@ -1,7 +0,0 @@
-use Mix.Config
-
-config :glass_factory_api, GlassFactoryApi.ApiClient,
-  subdomain: System.fetch_env!("GLASSFACTORY_SUBDOMAIN"),
-  user_token: System.fetch_env!("GLASSFACTORY_USER_TOKEN"),
-  user_email: System.fetch_env!("GLASSFACTORY_USER_EMAIL"),
-  api_url: System.fetch_env!("GLASSFACTORY_API_URL")
